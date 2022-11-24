df94e977a1399b21cc0fd23fbb1a8492c92e668c
Uladzislau_Maksimuk <uladzislau_maksimuk@epam.com>
2022-11-24 16:55:19 +0300

    Sequence preservation solution

--- src/main/java/com/kafka/course/k2/Producer.java
+++ src/main/java/com/kafka/course/k2/Producer.java
@@ -2,8 +2,6 @@ package com.kafka.course.k2;
 
 import com.kafka.course.k2.model.Reposition;
 import com.kafka.course.k2.model.Signal;
-import java.util.concurrent.CountDownLatch;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -17,14 +15,9 @@ public class Producer {
   @Autowired
   private KafkaTemplate<String, Reposition> kafkaTemplateRep;
 
-  private int key = 0;
-
   // 2. Producer: requirement “Messages from every vehicle must be processed sequentially!” Not implemented
   public void send(String topic, Signal signal) {
-    ProducerRecord<String, Signal> producerRecord =
-        new ProducerRecord<>(topic, Integer.toString(key), signal);
-    key++;
-    kafkaTemplateSig.send(topic, signal);
+    kafkaTemplateSig.send(topic, signal.getVehicleId(), signal);
   }
 
   public void send(String topic, Reposition reposition) {
